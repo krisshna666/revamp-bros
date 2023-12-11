5594234750090b44b6d6c8ea870a9a17356a6664
srikrishna <krishna199987@gmail.com>
2023-12-12 02:11:02 +0530

    worked on home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
+import { HeaderComponent } from './components/header/header.component';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  public clients = [
+    {
+      name: 'Bang Upin',
+      role: 'Pedagang Asongan',
+      review:
+        '“Terimakasih banyak, kini ruanganku menjadi lebih mewah dan terlihat mahal“',
+      image: '../../assets/images/person1.png',
+    },
+    {
+      name: 'Ibuk Sukijan',
+      role: 'Lorim Ipsum',
+      review:
+        '“Makasih Panto, aku sekarang berasa tinggal di apartment karena barang-barang yang terlihat mewah“',
+      image: '../../assets/images/person2.png',
+    },
+    {
+      name: 'Mpok Ina',
+      role: 'Lorim Ipsum',
+      review:
+        '“Sangat terjangkau untuk kantong saya yang tidak terlalu banyak“',
+      image: '../../assets/images/person3.png',
+    },
+  ];
   constructor() {}
-
 }
